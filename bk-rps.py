2761ba053718ee593aaa3c1db4d6473a7c9c2eba
Samuel <samuelsoares177778@gmail.com>
2019-08-22 20:05:15 -0300

    novos templates para testes no servidor

--- bk-rps.py
+++ bk-rps.py
@@ -1,9 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, session, flash, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('login.html')
+
+@app.route('/login')
+def login():
+    proxima = request.args.get('proxima')# variavel que foi definida no redirect
+    return render_template('login.html', proxima=proxima)
+
+@app.route('/autenticar', methods=['POST'])
+def autenticar():
+    pass
+    #aqui vai a logica que iremos usar para autenticar se o usuario esta no banco de dados 
 
 app.run(debug=True)
