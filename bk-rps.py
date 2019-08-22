6a2a66692f1523ffd8bb774d4b466e87e699b0e5
Samuel <samuelsoares177778@gmail.com>
2019-08-22 20:29:09 -0300

    novos metodos e urls pro servidor

--- bk-rps.py
+++ bk-rps.py
@@ -1,10 +1,39 @@
 from flask import Flask, render_template, redirect, request, session, flash, url_for
 
+
 app = Flask(__name__)
 
+class Aluno:
+    def __init__(self, nome, cpf, telefone): #atributos pra teste depois iremos incluir mais
+        self.__nome = nome #aqui o __ deixa o atributo privado
+        self.__cpf = cpf # sendo necessario getter e setter para buscar os atributos, se quiser pode tirar 
+        self.__telefone = telefone
+
+class Usuario:
+    def __init__(self, nome_user, senha, aluno):
+        self.__nome_user = nome_user
+        self.__senha = senha
+        self.__aluno = Aluno
+
+lista_alunos = []
 @app.route('/')
 def index():
-    return render_template('login.html')
+    return render_template('login.html', titulo='login')
+
+@app.route('/novo')
+def novo():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect(url_for('login', proxima= url_for('novo'))) #chama a função login e passa a proxima pagina como parametro
+    return render_template('aluno.html', titulo='Novo Aluno')
+
+@app.route('/criar', methods=['POST'])
+def criar():
+    nome = request.form['nome'] #name de nome no formulario
+    cpf = request.form['cpf']
+    telefone = request.form['telefone']
+    aluno = Aluno(nome, cpf, telefone)
+    lista_alunos.append(aluno)
+    return redirect(url_for('login'))
 
 @app.route('/login')
 def login():
