841d94e207d97e888cc44a454b0f7145caa6fe74
Samuel <samuelsoares177778@gmail.com>
2019-08-27 08:01:41 -0300

    admin adicionado

--- bk-rps.py
+++ bk-rps.py
@@ -2,7 +2,9 @@ from flask import Flask, render_template, redirect, request, session, flash, url
 
 
 app = Flask(__name__)
+
 app.secret_key = 'sessao'  # Aqui definida uma chave para salvar a sessão do usuario
+
 class Aluno:
     def __init__(self, nome, cpf, telefone): #atributos pra teste depois iremos incluir mais
         self.__nome = nome #aqui o __ deixa o atributo privado
@@ -15,15 +17,21 @@ class Usuario:
         self.__senha = senha
         self.__aluno = Aluno
 
-lista_alunos = []
+lista = []
+aluno = Aluno('samuel',121212,12121221)
+lista.append(aluno)
+
+@app.route('/admin')
+def admin():
+    return render_template('lista.html', titulo='usuarios', usuarios=lista)  
+
 @app.route('/')
 def index():
     return render_template('login.html', titulo='login')
 
 @app.route('/novo')
 def novo():
-    if 'usuario_logado' not in session or session['usuario_logado'] == None:
-        return redirect(url_for('login', proxima= url_for('novo'))) #chama a função login e passa a proxima pagina como parametro
+  #chama a função login e passa a proxima pagina como parametro
     return render_template('aluno.html', titulo='Novo Aluno')
 
 @app.route('/criar', methods=['POST'])
@@ -33,16 +41,16 @@ def criar():
     telefone = request.form['telefone']
     aluno = Aluno(nome, cpf, telefone)
     lista_alunos.append(aluno)
-    return redirect(url_for('login'))
+    return render_template('aluno.html')
 
 @app.route('/login')
 def login():
     proxima = request.args.get('proxima')# variavel que foi definida no redirect
     return render_template('login.html', proxima=proxima)
 
-@app.route('/autenticar', methods=['POST'])
+@app.route('/auth', methods=['POST'])
 def autenticar():
-    pass
-    #aqui vai a logica que iremos usar para autenticar se o usuario esta no banco de dados 
+    
+    
 
 app.run(debug=True)
